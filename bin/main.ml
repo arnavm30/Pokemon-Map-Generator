094c3a8c9854317ec862d2cbfacfd4f133e6ff2a
Orion <owt4@cornell.edu>
2023-03-19 11:33:00 -0400

    Button functionality exploration
    - explore how buttons can work before refactoring it out of main.ml
    - using filled rectangles as buttons
    - use wait_next_event to see where mouse presses are
    - draw black rectangle when "red button" is pressed

--- bin/main.ml
+++ bin/main.ml
@@ -1,14 +1,35 @@
 open Graphics
 
+(** handle button presses*)
+let rec button_press x =
+  let status = wait_next_event x in
+  let width = size_x () in
+  let height = size_y () in
+  if status.mouse_x >= width / 4 && status.mouse_x <= (width / 4) + (width / 2)
+  then
+    if
+      status.mouse_y >= height / 20
+      && status.mouse_y <= (height / 20) + (height / 10)
+    then (
+      set_color black;
+      fill_rect (width / 4) (height / 5) (width / 2) (height / 10))
+    else (
+      print_endline "hello";
+      button_press [ Button_down ])
+  else (
+    print_endline "hello2";
+    button_press [ Button_down ])
+
 (** [main ()] opens a graphics window*)
 let main () =
   open_graph "";
-  print_endline "";
-  print_int (size_y ());
-  print_endline "";
-  print_int (size_x ());
   (* window closes as soon as script terminates, so wait until you press
      a key to keep window open*)
+  set_color red;
+  let width = size_x () in
+  let height = size_y () in
+  fill_rect (width / 4) (height / 20) (width / 2) (height / 10);
+  button_press [ Button_down ];
   ignore (Graphics.read_key ())
 
 (* Execute the graphics engine. *)
