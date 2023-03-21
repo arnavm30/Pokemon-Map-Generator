ca9a7116a1c4a57f6ca086dfa0366c935814242c
Arnav Muthiayen <am2589@cornell.edu>
2023-03-20 23:51:46 -0400

    Added Util test cases
    - Added 2 test cases, 1 each for Util.sum and Util.entropy

--- test/main.ml
+++ test/main.ml
@@ -31,6 +31,14 @@ let array_div_test (name : string) (arr1 : float array) (arr2 : float array)
     (expected_output : float array) : test =
   array_op_test name arr1 arr2 ( ///. ) expected_output
 
+let sum_test (name : string) (flts : float array) (expected_output : float) :
+    test =
+  name >:: fun _ -> assert_equal expected_output (sum flts)
+
+let entropy_test (name : string) (w : float array) (expected_output : float) :
+    test =
+  name >:: fun _ -> assert_equal expected_output (entropy w)
+
 let tests =
   [
     array_add_test "array add 0s array is same array" (Array.make 10 5.)
@@ -41,6 +49,8 @@ let tests =
       (Array.make 10 1.) (Array.make 10 5.);
     array_div_test "array div 1s array is same array" (Array.make 10 5.)
       (Array.make 10 1.) (Array.make 10 5.);
+    sum_test "[] has 0 sum" (Array.make 0 0.) 0.;
+    entropy_test "[0.5] has 0 entropy" (Array.make 1 0.5) 0.;
   ]
 
 let test_suite = "generator test suite" >::: tests
